56d38f8829b501b59e351a81738cc76fd569c34b
Jonathan Simpson <jon@Jonathans-MacBook-Air.local>
2022-10-24 23:14:37 +0100

    Experimenting with styles

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,38 @@
+<!-- eslint-disable prettier/prettier -->
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <PassageGame msg="# PRESS ANY KEY" />
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
-import HelloWorld from "./components/HelloWorld.vue";
+import PassageGame from "./components/PassageGame.vue";
+import "@fontsource/press-start-2p";
 
 @Options({
   components: {
-    HelloWorld,
+    PassageGame,
   },
 })
 export default class App extends Vue {}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+body {
+  margin-top: 100px;
+  background-color: black;
+}
+h1 {
+  font-family: "Press Start 2P", monospace;
+  font-size: 22px;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #4cc005;
+  background-color: black;
+}
+table {
+  width: 80%;
+  margin: 0 auto;
+  border-style: solid;
+  border-color: #4cc005;
+  padding: 20px;
 }
 </style>
---
+++ src/components/PassageGame.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="main">
+    <table>
+      <tr>
+        <h1>{{ msg }}</h1>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+  props: {
+    msg: String,
+  },
+})
+export default class PassageGame extends Vue {
+  msg!: string;
+}
+</script>
