968b757d82fb6efdec83e0ae3e682ad4273c664b
Jonathan Simpson <jon@Jonathans-MacBook-Air.local>
2022-10-28 23:11:56 +0100

    CSS formatting spike and updates

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <!-- eslint-disable prettier/prettier -->
 <template>
-  <PassageGame msg="&> PRESS ANY KEY" />
+  <PassageGame msg="TEST"/>
   <PassageGameFooter />
 </template>
 
@@ -23,18 +23,17 @@ export default class App extends Vue {}
 body {
   background-color: black;
   background-image: radial-gradient(rgba(0, 150, 0, 0.75), black 120%);
+  width: 100vw;
   height: 100vh;
-  margin: 0;
   overflow: hidden;
-  padding: 2rem;
   color: rgb(15, 230, 15);
   font: 10px "Press Start 2p", monospace;
   text-shadow: 0 0 5px #182e15;
   pointer-events: none;
+  margin: 0;
 }
 
 body::after {
-  content: "";
   position: fixed;
   top: 0;
   left: 0;
@@ -54,11 +53,27 @@ body::after {
   text-shadow: none;
 }
 #app {
-  position: relative;
+  position: fixed;
   height: 100vh;
+  width: 100vw;
+  display: inline-block;
+}
+#main {
+  position: fixed;
+  width: 100vw;
+  height: 90vh;
+  padding: 2rem;
 }
 #footer {
-  position: absolute;
-  bottom: 50px;
+  position: fixed;
+  width: 100vw;
+  height: 10vh;
+  bottom: 0;
+  padding: 2rem;
+}
+h1,
+h2,
+h3 {
+  margin: 0;
 }
 </style>
